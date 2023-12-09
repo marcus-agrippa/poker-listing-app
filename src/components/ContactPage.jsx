447b5568577e30edfc832cc0c05c415e763b7cb3
marcus-agrippa <benjamincowan1@gmail.com>
2023-12-09 14:37:00 +1100

    form updates

--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -1,34 +1,76 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const ContactPage = () => {
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const myForm = event.target;
+    const formData = new FormData(myForm);
+
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
+    })
+    .then(() => {
+      setSubmitted(true);
+      myForm.reset(); // Reset the form fields
+    })
+    .catch((error) => {
+      alert(error);
+      setSubmitted(false);
+    });
+  };
+
   return (
     <div className="container mx-auto p-4">
-    <h1 className="text-3xl text-white font-bold text-center mb-6">Contact Us</h1>
-        <form name="contact" method="POST" data-netlify="true" className="max-w-lg mx-auto">
+      <h1 className="text-3xl text-white font-bold text-center mb-6">Contact Us</h1>
+      {!submitted ? (
+        <form 
+          name="contact" 
+          method="POST" 
+          data-netlify="true" 
+          onSubmit={handleSubmit} 
+          className="max-w-lg mx-auto"
+        >
+          <input type="hidden" name="form-name" value="contact" />
+
           <div className="mb-4">
-            <label htmlFor="name" className="block mb-2 text-white text-md font-medium text-gray-700">Your Name:</label>
-            <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
+            <label htmlFor="name" className="block mb-2 text-md text-white font-medium text-gray-700">Your Name:</label>
+            <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
           </div>
+
           <div className="mb-4">
-            <label htmlFor="contactEmail" className="block mb-2 text-white text-md font-medium text-gray-700">Your Email (optional):</label>
+            <label htmlFor="contactEmail" className="block mb-2 text-md text-white font-medium text-gray-700">Your Email (optional):</label>
             <input type="email" name="contactEmail" id="contactEmail" placeholder="you@example.com" className="bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
           </div>
+
           <div className="mb-4">
-            <label htmlFor="game" className="block mb-2 text-white text-md font-medium text-gray-700">Game:</label>
+            <label htmlFor="game" className="block mb-2 text-md text-white font-medium text-gray-700">Game:</label>
             <textarea name="game" id="game" className="bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" rows="4"></textarea>
           </div>
+
           <div className="mb-4">
-            <label htmlFor="message" className="block mb-2 text-white text-md font-medium text-gray-700">Message:</label>
+            <label htmlFor="message" className="block mb-2 text-md text-white font-medium text-gray-700">Message:</label>
             <textarea name="message" id="message" className="bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"></textarea>
           </div>
+
           <div className="text-center">
             <button type="submit" className="btn btn-primary">Send</button>
           </div>
         </form>
-      </div>
-  )
-}
+      ) : (
+        <div className="text-center text-green-300">
+          <p>Thank you for contacting us. We will get back to you soon!</p>
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default ContactPage
+export default ContactPage;
+
+
 
 

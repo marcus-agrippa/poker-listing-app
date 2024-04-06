40b0a9ec6440be96d58714696f49944269c6443f
marcus-agrippa <benjamincowan1@gmail.com>
2024-04-06 13:01:11 +1100

    update header to handle new url conditions

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { GiPokerHand } from 'react-icons/gi'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { GiPokerHand } from 'react-icons/gi';
+import { hostname } from '../../hostname';
 
 const Header = () => {
+  const title =
+    hostname === 'pokercentralcoast.com'
+      ? 'POKER CENTRAL COAST'
+      : hostname === 'pokernewcastle.com'
+      ? 'POKER NEWCASTLE'
+      : 'POKER';
+
   return (
     <div className='navbar bg-slate-800 text-center justify-center'>
       <Link to='/' className='text-xl text-white font-bold py-4'>
-        <GiPokerHand className='text-5xl mr-3' /> POKER CENTRAL COAST
+        <GiPokerHand className='text-5xl mr-3' /> {title}
       </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;

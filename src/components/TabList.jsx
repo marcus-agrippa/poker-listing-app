e4f0252cc9306c0e95a061ac1aa8d81f762b5734
marcus-agrippa <benjamincowan1@gmail.com>
2024-04-13 08:56:45 +1000

    update central coast game data

--- src/components/TabList.jsx
+++ src/components/TabList.jsx
@@ -22,7 +22,7 @@ const TabList = ({ activeDay, setActiveDay, daysOfWeek }) => {
         Location: <br></br>
         <span className='text-xl text-blue-500 font-bold'>{location}</span>
       </p>
-      <LastUpdated lastUpdated='April 7th, 2024' />
+      <LastUpdated lastUpdated='April 13th, 2024' />
       <br></br>
       <div className='lg:hidden mb-4'>
         <label

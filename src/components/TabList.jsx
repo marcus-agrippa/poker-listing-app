72e6d90fdb6fa06e40b931dace78181de48e3717
marcus-agrippa <benjamincowan1@gmail.com>
2024-05-18 17:12:51 +1000

    update last updated date

--- src/components/TabList.jsx
+++ src/components/TabList.jsx
@@ -32,7 +32,7 @@ const TabList = ({ activeDay, setActiveDay, daysOfWeek }) => {
         Location: <br></br>
         <span className='text-xl text-blue-500 font-bold'>{location}</span>
       </p>
-      <LastUpdated lastUpdated='May 4th, 2024' />
+      <LastUpdated lastUpdated='May 18th, 2024' />
       <br></br>
       <div className='lg:hidden mb-4'>
         <label

d634a54af56114497454eda4eb36dff9bafe82d8
marcus-agrippa <benjamincowan1@gmail.com>
2024-06-01 10:37:10 +1000

    add last updated today function logic

--- src/components/ui/LastUpdated.jsx
+++ src/components/ui/LastUpdated.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 
 const LastUpdated = ({ lastUpdated }) => {
+  const today = new Date();
+  const cleanedLastUpdated = lastUpdated.replace(/(\d+)(st|nd|rd|th)/, '$1');
+  const updatedDate = new Date(cleanedLastUpdated);
+
+  const updatedToday =
+    updatedDate.getDate() === today.getDate() &&
+    updatedDate.getMonth() === today.getMonth() &&
+    updatedDate.getFullYear() === today.getFullYear()
+      ? 'Today'
+      : lastUpdated;
+
   return (
     <div className='flex items-center justify-center'>
       <div className='w-2.5 h-2.5 bg-green-500 rounded-full animate-pulse-green'></div>
-      <span className='ml-2 text-neutral-400'>Last Updated: {lastUpdated}</span>
+      <span className='ml-2 text-neutral-400'>
+        Last Updated: {updatedToday}
+      </span>
     </div>
   );
 };

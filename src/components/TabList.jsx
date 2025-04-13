a6ad88a261f9ae174838fafc65f2029d2151f4b6
marcus-agrippa <benjamincowan1@gmail.com>
2025-04-13 20:03:09 +1000

    update games

--- src/components/TabList.jsx
+++ src/components/TabList.jsx
@@ -36,7 +36,7 @@ const TabList = ({ activeDay, setActiveDay, daysOfWeek }) => {
         Location: <br></br>
         <span className='text-xl text-blue-500 font-bold'>{location}</span>
       </p>
-      <LastUpdated lastUpdated='April 3, 2025' />
+      <LastUpdated lastUpdated='April 13, 2025' />
       <br></br>
       <div className='lg:hidden mb-4'>
         <label
